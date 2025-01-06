2dfb356e805d22b4951364289fd45aba0871cac6
Sumei <slwoo1@asu.edu>
2025-01-06 16:17:04 -0700

    Update App.vue file

--- src/App.vue
+++ src/App.vue
@@ -27,31 +27,34 @@
     
     <!-- Testing with button navigation to view all pages without submenu (submenu not yet implemented) -->
     <nav>
-      <button @click="setPage('adminlogin')">Admin Login</button>
-      <button @click="setPage('adminhome')">Home</button>
-      <button @click="setPage('adminteaching')">Teaching</button>
-      <button @click="setPage('adminpublications')">Publications</button>
-      <button @click="setPage('adminresearch')">Research</button>
-      <button @click="setPage('admininsideasu')">Inside ASU</button>  <!-- Public Work -->
-      <button @click="setPage('adminoutsideasu')">Outside ASU</button>  <!-- Public Work -->
-      <button @click="setPage('adminmentorfaculty')">Mentor Faculty</button>  <!-- Public Work -->
-      <button @click="setPage('adminmentorstudents')">Mentor Students</button>  <!-- Public Work -->
-      <button @click="setPage('adminworkhistory')">Work History</button>  <!-- Public Work -->
-      <button @click="setPage('admintools')">Tools</button>
-      <button @click="setPage('adminusers')">Admin Users</button>
-      <button @click="setPage('adminnew')">New Admin</button>
-      <button @click="setPage('studenthome')">Student Home</button>
-      <button @click="setPage('studentteaching')">Student Teaching</button>
-      <button @click="setPage('studentpublications')">Student Publications</button>
-      <button @click="setPage('studentresearch')">Student Research</button>
-      <button @click="setPage('studentinsideasu')">Student Inside ASU</button>
-      <button @click="setPage('studentoutsideasu')">Student Outside ASU</button>  <!-- Public Work -->
-      <button @click="setPage('studentmentorfaculty')">Student Mentor Faculty</button>  <!-- Public Work -->
-      <button @click="setPage('studentmentorstudents')">Student Mentor Students</button>  <!-- Public Work -->
-      <button @click="setPage('studentworkhistory')">Student Work History</button>  <!-- Public Work -->
-      <button @click="setPage('studenttools')">Student Tools</button>
+      <!-- Admin -->
+      <button @click="setPage('adminlogin')">A-Login</button>
+      <button @click="setPage('adminhome')">A-Home</button>
+      <button @click="setPage('adminteaching')">A-Teaching</button>
+      <button @click="setPage('adminpublications')">A-Publications</button>
+      <button @click="setPage('adminresearch')">A-Research</button>
+      <button @click="setPage('admininsideasu')">A-Inside ASU</button>  <!-- Public Work -->
+      <button @click="setPage('adminoutsideasu')">A-Outside ASU</button>  <!-- Public Work -->
+      <button @click="setPage('adminmentorfaculty')">A-Mentor Faculty</button>  <!-- Public Work -->
+      <button @click="setPage('adminmentorstudents')">A-Mentor Students</button>  <!-- Public Work -->
+      <button @click="setPage('adminworkhistory')">A-Work History</button>  <!-- Public Work -->
+      <button @click="setPage('admintools')">A-Tools</button>
+      <button @click="setPage('adminusers')">A-Admin Users</button>
+      <button @click="setPage('adminnew')">A-New Admin</button>
+      <!-- Public -->
+      <button @click="setPage('studenthome')">S-Home</button>
+      <button @click="setPage('studentteaching')">S-Teaching</button>
+      <button @click="setPage('studentpublications')">S-Publications</button>
+      <button @click="setPage('studentresearch')">S-Research</button>
+      <button @click="setPage('studentinsideasu')">S-Inside ASU</button>  <!-- Public Work -->
+      <button @click="setPage('studentoutsideasu')">S-Outside ASU</button>  <!-- Public Work -->
+      <button @click="setPage('studentmentorfaculty')">S-Mentor Faculty</button>  <!-- Public Work -->
+      <button @click="setPage('studentmentorstudents')">S-Mentor Students</button>  <!-- Public Work -->
+      <button @click="setPage('studentworkhistory')">S-Work History</button>  <!-- Public Work -->
+      <button @click="setPage('studenttools')">S-Tools</button>
     </nav>
 
+    <!-- Admin -->
     <AdminLogin v-if="currentPage === 'adminlogin'" />
     <AdminHome v-if="currentPage === 'adminhome'" />
     <AdminTeaching v-if="currentPage === 'adminteaching'" />
@@ -65,21 +68,23 @@
     <AdminTools v-if="currentPage === 'admintools'" />
     <AdminUsers v-if="currentPage === 'adminusers'" />
     <AdminNew v-if="currentPage === 'adminnew'" />
+    <!-- Public -->
     <StudentHome v-if="currentPage === 'studenthome'" />
-    <StudentInsideASU v-if="currentPage === 'studentinsideasu'" />  <!-- Public Work -->
-    <StudentMentorFaculty v-if="currentPage === 'studentmentorfaculty'" />  <!-- Public Work -->
-    <StudentMentorStudents v-if="currentPage === 'studentmentorstudents'" />  <!-- Public Work -->
-    <StudentOutsideASU v-if="currentPage === 'studentoutsideasu'" />  <!-- Public Work -->
+    <StudentTeaching v-if="currentPage === 'studentteaching'" />
     <StudentPublications v-if="currentPage === 'studentpublications'" />
     <StudentResearch v-if="currentPage === 'studentresearch'" />
-    <StudentTeaching v-if="currentPage === 'studentteaching'" />
-    <StudentTools v-if="currentPage === 'studenttools'" />
+    <StudentInsideASU v-if="currentPage === 'studentinsideasu'" />  <!-- Public Work -->
+    <StudentOutsideASU v-if="currentPage === 'studentoutsideasu'" />  <!-- Public Work -->
+    <StudentMentorFaculty v-if="currentPage === 'studentmentorfaculty'" />  <!-- Public Work -->
+    <StudentMentorStudents v-if="currentPage === 'studentmentorstudents'" />  <!-- Public Work -->
     <StudentWorkHistory v-if="currentPage === 'studentworkhistory'" />  <!-- Public Work -->
+    <StudentTools v-if="currentPage === 'studenttools'" />
   </div>
 </template>
 
 <script>
  /* Import the components */
+  // Admin
   import AdminLogin from './components/AdminLogin.vue';
   import AdminHome from './components/AdminHome.vue';
   import AdminTeaching from './components/AdminTeaching.vue';
@@ -93,31 +98,23 @@
   import AdminTools from './components/AdminTools.vue';
   import AdminUsers from './components/AdminUsers.vue';
   import AdminNew from './components/AdminNew.vue';
- import StudentHome from "@/components/StudentHome.vue";
- import StudentInsideASU from './components/StudentInsideASU.vue'; // Public Work
- import StudentMentorFaculty from './components/StudentMentorFaculty.vue'; // Public Work
- import StudentMentorStudents from './components/StudentMentorStudents.vue'; // Public Work
- import StudentOutsideASU from './components/StudentOutsideASU.vue'; // Public Work
- import StudentPublications from './components/StudentPublications.vue';
- import StudentResearch from './components/StudentResearch.vue';
- import StudentTeaching from './components/StudentTeaching.vue';
- import StudentTools from './components/StudentTools.vue';
- import StudentWorkHistory from './components/StudentWorkHistory.vue'; // Public Work
+  // Public
+  import StudentHome from "@/components/StudentHome.vue";
+  import StudentTeaching from './components/StudentTeaching.vue';
+  import StudentPublications from './components/StudentPublications.vue';
+  import StudentResearch from './components/StudentResearch.vue';
+  import StudentInsideASU from './components/StudentInsideASU.vue'; // Public Work
+  import StudentOutsideASU from './components/StudentOutsideASU.vue'; // Public Work
+  import StudentMentorFaculty from './components/StudentMentorFaculty.vue'; // Public Work
+  import StudentMentorStudents from './components/StudentMentorStudents.vue'; // Public Work
+  import StudentWorkHistory from './components/StudentWorkHistory.vue'; // Public Work
+  import StudentTools from './components/StudentTools.vue';
 
   export default {
     name: 'App',
     components: {
-      StudentWorkHistory,
-      StudentTools,
-      StudentTeaching,
-      StudentResearch,
-      StudentPublications,
-      StudentMentorFaculty,
-      StudentHome,
-      StudentInsideASU,
-      StudentMentorStudents,
-      StudentOutsideASU,
       /* Register components */
+      // Admin
       AdminLogin,
       AdminHome,
       AdminTeaching,
@@ -131,6 +128,17 @@
       AdminTools,
       AdminUsers,
       AdminNew,
+      // Public
+      StudentHome,
+      StudentTeaching,
+      StudentPublications,
+      StudentResearch,
+      StudentInsideASU,
+      StudentOutsideASU,
+      StudentMentorFaculty,
+      StudentMentorStudents,
+      StudentWorkHistory,
+      StudentTools,
     },
 
     data() {
@@ -150,7 +158,7 @@
 </script>
 
 <style>
-/* Import CSS files if needed*/
+/* Import CSS files if needed */
 /*
 e.g.,
 @import './assets/globals.css';
