ad37f492386f2cfc8c740d75f9e820c65f3d6ad4
eross11 <eross11@asu.edu>
2025-04-07 17:21:40 -0700

    .env and router.js changes for callback

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory} from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import { authGuard } from '@auth0/auth0-vue';
 
 import UserLogin from './pages/UserLogin.vue';
@@ -60,7 +60,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 
